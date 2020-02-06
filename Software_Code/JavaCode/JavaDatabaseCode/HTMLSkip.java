e3ad0affd31371d168e110c2a2d521a1f9f79d4e
Ryan <Ryan@LAPTOP-HQAH8KNT>
2020-02-06 14:49:10 +0000

    Update HTMLSkip.java
    Fixing Details

--- Software_Code/JavaCode/JavaDatabaseCode/HTMLSkip.java
+++ Software_Code/JavaCode/JavaDatabaseCode/HTMLSkip.java
@@ -18,31 +18,38 @@ public class HTMLSkip
 	//This method was inspired by http://www.javapractices.com/topic/TopicAction.do?Id=96 In which it is based off
 	public static String forHTML(String aText)
 	{
-	     StringBuilder r = new StringBuilder();
-	     StringCharacterIterator iterator = new StringCharacterIterator(aText);
-	     char c =  iterator.current();
-	     while (c != CharacterIterator.DONE ){
-	       if (c == '<') {
-	         r.append("&lt;");
-	       }
-	       else if (c == '>') {
-	         r.append("&gt;");
-	       }
-	       else if (c == '&') {
-	         r.append("&amp;");
-	      }
-	       else if (c == '\"') {
-	         r.append("&quot;");
-	       }
-	       else 
-	       {
-	         //the char is not a special one
-	         //add it to the result as is
-	         r.append(c);
-	       }
-	       c = iterator.next();
-	     }
-	     System.out.println(r.toString());
-	     return r.toString();
+		//This will take in the text the system is trying to parse, this will take the input, and filter through the characters
+	    StringBuilder r = new StringBuilder();
+	    StringCharacterIterator iterator = new StringCharacterIterator(aText);
+	    char c =  iterator.current();
+		//While the character is not the last, check for any escapes required
+	    while (c != CharacterIterator.DONE )
+		{
+	       	if (c == '<') {
+	         	r.append("&lt;");
+	       	}
+	       	else if (c == '>') 
+			{
+	         	r.append("&gt;");
+	       	}
+	       	else if (c == '&') 
+		   	{
+	        	r.append("&amp;");
+	      	}
+	       	else if (c == '\"') 
+			{
+	        	r.append("&quot;");
+	       	}
+	       	else 
+	       	{
+	        //the char is not a special one
+	        //add it to the result as is
+	        	r.append(c);
+	       	}
+	       	c = iterator.next();
+	    }
+		//Method Check
+	    System.out.println(r.toString());
+	    return r.toString();
 	}
 }
